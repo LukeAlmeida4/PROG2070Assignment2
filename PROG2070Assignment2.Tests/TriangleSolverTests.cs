21da65aff3d2afdfe286c5f43191bfc2a4cca710
LukeAlmeida4 <luke-almeida@hotmail.com>
2018-02-19 11:24:02 -0500

    Final Commit

---
+++ PROG2070Assignment2.Tests/TriangleSolverTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PROG2070Assignment2.Tests
+{
+    [TestFixture]
+    class TriangleSolverTests
+    {
+        //Testing the analyze method to see if it will correctly check whether three sides form a proper triangle.
+        //Three integers are passed through to the method, and two of the sides added must be greater than the third.
+        //This test will check the first two integers added compared to the third length
+        [Test]
+        public void AnalyzeMethodTest_LengthsOneTwoTriangleNotFormed_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "Sorry this is not a proper triangle - returning to main menu";
+            actual = TriangleSolver.Analyze(2, 3, 8); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+
+        [Test]
+        //Testing the analyze method to see if it will correctly check whether three sides form a proper triangle.
+        //Three integers are passed through to the method, and two of the sides added must be greater than the third.
+        //This test will check the first and third integers added compared to the second length
+        public void AnalyzeMethodTest_LengthsOneThreeTriangleNotFormed_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "Sorry this is not a proper triangle - returning to main menu";
+            actual = TriangleSolver.Analyze(2, 8, 3); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+
+        //Testing the analyze method to see if it will correctly check whether three sides form a proper triangle.
+        //Three integers are passed through to the method, and two of the sides added must be greater than the third.
+        //This test will check the second and third integers added compared to the first length
+        [Test]
+        public void AnalyzeMethodTest_LengthsTwoThreeTriangleNotFormed_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "Sorry this is not a proper triangle - returning to main menu";
+            actual = TriangleSolver.Analyze(8, 3, 2); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+
+        //Testing the analyze method to see if it will return the strong confirming an equilateral triangle. This is one of the possible
+        //routes of execution for the method based on the integers passed through. All sides must be the same in length for it to return the expected string
+        [Test]
+        public void AnalyzeMethodTest_EquilateralCheck_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "This correctly forms a triangle, thanks! After checking the numbers I have determined this is an Equilateral Triangle ";
+            actual = TriangleSolver.Analyze(2,2,2); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+
+        //Testing the analyze method to see if it will return the string confirming a scalene triangle. This is one of the possible
+        //routes of execution for the method based on the integers passed through. All sides must be different in length for the expected string to return
+        [Test]
+        public void AnalyzeMethodTest_ScaleneCheck_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "This correctly forms a triangle, thanks! After checking the numbers I have determined this is a Scalene Triangle ";
+            actual = TriangleSolver.Analyze(4, 9, 11); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+
+        //Testing the analyze method to see if it will return the string confirming a isosceles triangle. This is one of the possible
+        //routes of execution for the method based on the integers passed through. Two sides must be the same in length for the expected string to return
+        [Test]
+        public void AnalyzeMethodTest_IsoscelesCheck_ReturnsString()
+        {
+            string actual; //Arrange
+            string expected;
+
+            expected = "This correctly forms a triangle, thanks! After checking the numbers I have determined this is an Isosceles Triangle ";
+            actual = TriangleSolver.Analyze(6, 8, 8); //Act
+
+            Assert.AreEqual(expected, actual); //Assert
+        }
+        //Testing the analyze method when input is 0
+        //0 can not be used to form a valid triangle, and our program is meant to stop 0 from passing through 
+        // this test ensures the argument is thrown
+        [Test]
+        public void AnalyzeMethodTest_WhenInputIs0_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => TriangleSolver.Analyze(0, 0, 0));
+        }
+        //Testing the analyze method when input is negative
+        //negative values cannot be used to form a valid triangle, and our program is meant to stop 0 from passing through 
+        //this test ensures the argument is thrown
+        [Test]
+        public void AnalyzeMethodTest_WhenInputIsNegative_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => TriangleSolver.Analyze(-2, -2, -2));
+        }
+    }
+}
---
+++ PROG2070Assignment2/TriangleSolver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assignment2
+{
+    public class TriangleSolver
+    {
+
+        public static string Analyze(int x, int y, int z)
+        {
+            //Declaring our variables
+            string analyzedOutput = null;
+            string triangleString = null;
+            bool triangleCheck = false;
+
+            //checking to see if the passed values form a triangle
+            if (x + y > z && x + z > y && y + z > x)
+            {
+                //if they do returh a string confirming this and change the boolean variable to true
+                triangleString = "This correctly forms a triangle, thanks! ";
+                triangleCheck = true;
+            }
+            else
+            {
+                //if not change the boolean variable to false
+                triangleCheck = false;
+            }
+
+            //excude the code if the boolean is true
+            if (triangleCheck == true)
+            {
+                //checking for an equilateral triangle with the values
+                if (x == y && y == z && z == x)
+                {
+                    analyzedOutput = triangleString + "After checking the numbers I have determined this is an Equilateral Triangle ";
+                }
+                //checking for a scalene triangle with the values
+                else if (x != y && y != z && z != x)
+                {
+                    analyzedOutput = triangleString + "After checking the numbers I have determined this is a Scalene Triangle ";
+                }
+                //checking for a isosceles triangle with the values
+                else if (x == y || x == z || y == z)
+                {
+                    analyzedOutput = triangleString + "After checking the numbers I have determined this is an Isosceles Triangle ";
+                }
+            }
+            //execute this code if the bool is false
+            else 
+            {
+                analyzedOutput = "Sorry this is not a proper triangle - returning to main menu";
+            }
+            //checking if the passed value is null
+            if (x == 0)
+            {
+                throw new ArgumentNullException("x");
+            }
+            //checking if the passed value is negative
+            if (x < -1)
+            {
+                throw new ArgumentException("x");
+            }
+
+            return analyzedOutput;
+
+        }
+    }
+}
